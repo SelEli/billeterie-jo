6a8923a842ba816910cb1da370132bb292364678
SelEli <seleli@live>
2025-09-25 14:03:20 +0200

    Verification: 70% + Strong PWD 90%

--- frontend/src/auth/pages/AuthRegister.jsx
+++ frontend/src/auth/pages/AuthRegister.jsx
@@ -26,9 +26,17 @@ export default function AuthRegister() {
   return (
     <PageLayout title="Inscription" containerSize="md" gap="4">
       <AuthForm mode="register" onSubmit={handleRegister} />
+
+      {/* Bloc consigne mot de passe */}
+      <div className="cta-login">
+        🔒 Votre mot de passe doit contenir au moins 12 caractères, 
+        avec majuscule, minuscule, chiffre et caractère spécial.
+      </div>
+
       <p className="text-white drop-shadow text-center">
         Déjà un compte ? <Link to="/login" className="underline">Connectez-vous</Link>
       </p>
     </PageLayout>
+
   );
 }
